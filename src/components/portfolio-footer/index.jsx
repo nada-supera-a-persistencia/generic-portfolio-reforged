a0c80819cc92c4bd1c177b91c0ccd12745152a0a
ODAIR JUNIOR DO ROSARIO <137020257+OdairRosario@users.noreply.github.com>
2024-04-07 00:20:28 -0300

    feat(footer-component): :art: Criado componente do footer separadamente e removido footer antigo

---
+++ src/components/portfolio-footer/index.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function PortfolioFooter() {
+    return (
+        <footer>
+            <p className="rodape">@2024 - PortfolioExemple</p>
+        </footer>
+    )
+}
