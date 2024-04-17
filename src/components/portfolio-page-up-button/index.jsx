dcece2336976a41d0b2fb28a68f7ad41cbabaf0c
Douglas Resende <douglasmresende@gmail.com>
2024-04-16 22:59:58 -0300

    fix(componente-arrow-up): :bug: Ajuste nomeclatura
    Closes #2

--- src/components/portfolio-page-up-button/index.jsx
+++ src/components/portfolio-page-up-button/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaArrowUp } from "react-icons/fa";
-import "./style.css";
+import "./styles.css";
 
 export default function PortfolioPageUpButton() {
   return (
