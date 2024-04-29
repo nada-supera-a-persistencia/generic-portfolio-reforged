0043a7e37921461857a49cf605c88072268ce0b7
Douglas Resende <douglasmresende@gmail.com>
2024-04-29 20:35:20 -0300

    fix(componente-header): :bug: Ajuste, remoção do fragmento
    1

--- src/components/portifolio-header/index.jsx
+++ src/components/portifolio-header/index.jsx
@@ -2,21 +2,19 @@ import './styles.css';
 
 export default function PortfolioHeader() {
   return (
-    <>
-      <div className="top">
-        <a className="link" href="#home">
-          Inicio
-        </a>
-        <a className="link" href="#projects">
-          Projetos
-        </a>
-        <a className="link" href="#about">
-          Sobre
-        </a>
-        <a className="link" href="#stacks">
-          Tecnologias
-        </a>
-      </div>
-    </>
+    <div>
+      <a className="link" href="#home">
+        Inicio
+      </a>
+      <a className="link" href="#projects">
+        Projetos
+      </a>
+      <a className="link" href="#about">
+        Sobre
+      </a>
+      <a className="link" href="#stacks">
+        Tecnologias
+      </a>
+    </div>
   );
 }
