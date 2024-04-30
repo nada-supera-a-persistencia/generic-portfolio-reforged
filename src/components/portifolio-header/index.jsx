b820065bf2aa1bb8293767fdb2f58229997c7fa5
Douglas Resende <81588964+dmresende@users.noreply.github.com>
2024-04-29 21:14:10 -0300

    [Feat] Criar Componente Header do Projeto (#11)
    * feat(componente-header): :sparkles: Criado componente para o header do projeto, removendo as tags no app.jsx

---
+++ src/components/portifolio-header/index.jsx
@@ -0,0 +1,20 @@
+import './styles.css';
+
+export default function PortfolioHeader() {
+  return (
+    <div>
+      <a className="link" href="#home">
+        Inicio
+      </a>
+      <a className="link" href="#projects">
+        Projetos
+      </a>
+      <a className="link" href="#about">
+        Sobre
+      </a>
+      <a className="link" href="#stacks">
+        Tecnologias
+      </a>
+    </div>
+  );
+}
