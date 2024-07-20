3adc8d580bb200ff81b8087f17d7183754ddf79c
ODAIR JUNIOR DO ROSARIO <10days66@gmail.com>
2024-07-20 22:22:07 +0000

    feat(refatoração-diversos-módulos): :sparkles: Removendo toda informação fixa do código e pegando de um arquivo

--- src/components/portfolio-stacks/index.jsx
+++ src/components/portfolio-stacks/index.jsx
@@ -1,32 +1,24 @@
-import { FaCss3Alt, FaHtml5, FaReact } from 'react-icons/fa';
-import { IoLogoJavascript } from 'react-icons/io5';
-import {
-  SiCsharp,
-  SiExpo,
-  SiMicrosoftsqlserver,
-  SiPostgresql,
-  SiTypescript,
-} from 'react-icons/si';
+import portfolioData from '../../../data.json';
 import './styles.css';
 
 export default function PortfolioStacks() {
   const iconSize = 50;
+  const { techStackIcons } = portfolioData;
 
   return (
     <>
       <h1 className="title" id="stacks">
-        TECNOLOGIAS
+        Techs
       </h1>
       <div className="icon-group">
-        <FaHtml5 color="var(--color-html)" size={iconSize} />
-        <FaCss3Alt color="var(--color-css)" size={iconSize} />
-        <IoLogoJavascript color="var(--color-js)" size={iconSize} />
-        <FaReact color="var(--color-react)" size={iconSize} />
-        <SiTypescript color="var(--color-ts)" size={iconSize} />
-        <SiExpo color="var(--color-expo)" size={iconSize} />
-        <SiCsharp color="var(--color-csharp)" size={iconSize} />
-        <SiMicrosoftsqlserver color="var(--color-sql-server)" size={iconSize} />
-        <SiPostgresql color="var(--color-postgress-icon)" size={iconSize} />
+        {techStackIcons.map((iconPath, index) => (
+          <img
+            key={index}
+            src={iconPath}
+            alt={`Ícone de tecnologia ${index + 1}`}
+            style={{ width: iconSize, height: iconSize }}
+          />
+        ))}
       </div>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <link
+      rel="stylesheet"
+      href="https://cdn.es.gov.br/fonts/font-awesome/css/font-awesome.min.css"
+    />
     <App />
   </React.StrictMode>,
 );
